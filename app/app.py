6d49633f37ae43762575ac72e7ac0d302bdbe34c
Adithyan A <deepaadithyan56@gmail.com>
2021-12-06 11:11:13 +0530

    test updated

--- app/app.py
+++ app/app.py
@@ -25,21 +25,24 @@ def home():
 
 @app.route("/convert", methods=['POST', 'GET'])
 def convert():
-    def delete():
-        directory = app.config['AUDIO_FOLDER']
-        files_in_directory = os.listdir(directory)
-        print(f"filesindirectory {files_in_directory}")
-        filtered_files = [file for file in files_in_directory if file.endswith(".mp3")]
-        print(f"Filtered LIst {filtered_files}")
-        for file in filtered_files:
-            print(f"- {file}")
-	        path_to_file = os.path.join(directory, file)
-            print(f"     path {path_to_file}")
-            os.remove(path_to_file)
-
     text = request.args.get('text_to_convert')
     date = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
     filename = "audio_" + date + ".mp3"
-    delete()
+
+    directory = app.config['AUDIO_FOLDER']
+    files_in_directory = os.listdir(directory)
+    print(f"filesindirectory {files_in_directory}")
+    filtered_files = [file for file in files_in_directory if file.endswith(".mp3")]
+    print(f"Filtered LIst {filtered_files}")
+    for file in filtered_files:
+        print(f"- {file}")
+        path_to_file = os.path.join(directory, file)
+        print(f"     path {path_to_file}")
+        os.remove(path_to_file)
+
+
+
+
+
     create_audio(text, app.config['AUDIO_FOLDER'] + filename)
     return redirect(url_for("home", text=text, file=filename))
